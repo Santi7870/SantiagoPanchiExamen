cb212aeb35f34d3af762238c3236f1c67919f188
Santi7870 <santiago.panchi@udla.edu.ec>
2024-11-29 07:09:38 -0500

    Agregar archivos de proyecto.

---
+++ SantiagoPanchiExamen/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SantiagoPanchiExamen
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SantiagoPanchiExamen/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SantiagoPanchiExamen
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
